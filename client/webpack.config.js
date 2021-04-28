83fcfcee822616227916b2fe6cdf7700a71ce233
KevvKo <kklein915@gmail.com>
2021-04-28 20:58:53 +0200

    allow webpack-dev-server and HtmlWebpackPlgin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,13 +1,31 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+const webpack = require('webpack'); 
+
 module.exports = {
-    mode: 'development',
-    entry: './src/scripts/main.js',
-    output: {
-      filename: 'main.js',
-      path: path.resolve(__dirname, 'dist'),
+
+    entry: {
+      main: './src/scripts/main.js',
     },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+  
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({template: "./public/index.html"})
+    ],
     devtool: 'inline-source-map',
     devServer: {
-        contentBase: './index.html',
+        contentBase: './public/index.html',
       },
   };
