e91a5f5a7d24e3153e91207a77eb9839ca9e28f1
KevvKo <kklein915@gmail.com>
2021-05-05 20:11:25 +0200

    remoe ERC20-Import

--- contracts/MWallet.sol
+++ contracts/MWallet.sol
@@ -1,33 +1,37 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.4.0 <0.9.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
 contract MWallet {
     address owner;
 
     event Deposit(address _sender, uint _amount);
     event Withdraw(address _sender, uint _amount);
     event Received(address _sender, uint _amount);
+    event Test(string message);
 
     constructor(){
         owner = msg.sender;
+        emit Test("created joo");
+
     }
 
-    fallback () external {
+    fallback () external payable{
+        emit Test("received");
         emit Received( msg.sender, msg.value);
+
     }
 
     function sendFunds(address recipient, uint amount) public payable{
         require(msg.sender == owner, "Transaction denied: sender is has no permissions!");
         require(owner.balance < amount, "Transaction denied: not enough wei to send!");
 
-        IERC20(owner).transferFrom(owner, recipient, amount);
         emit Withdraw(owner, amount);
     }
 
-    function currentBalance() public view returns(uint256){ 
+    function send() public payable {}
+
+    function currentBalance() public view returns(address, uint256){ 
         require(msg.sender == owner, "Request denied: Caller is not the owner");
-        return IERC20(owner).balanceOf(owner);
+        return (owner, address(this).balance);
     }
 }
