0a22cf79de5f6dc778f4124e3e0155a2d77b0dde
KevvKo <kklein915@gmail.com>
2021-05-05 23:32:58 +0200

    define new events and finish functions

--- contracts/MWallet.sol
+++ contracts/MWallet.sol
@@ -4,33 +4,30 @@ pragma solidity >=0.4.0 <0.9.0;
 contract MWallet {
     address owner;
 
-    event Deposit(address _sender, uint _amount);
     event Withdraw(address _sender, uint _amount);
     event Received(address _sender, uint _amount);
-    event Test(string message);
+    event Call(address _sender, string message);
 
     constructor(){
         owner = msg.sender;
-        emit Test("created joo");
-
     }
 
-    fallback () external payable{
-        emit Test("received");
-        emit Received( msg.sender, msg.value);
-
+    fallback() external payable{
+        emit Call(msg.sender, "Call was successful");
     }
 
-    function sendFunds(address recipient, uint amount) public payable{
+    receive() external payable{
+        emit Received(msg.sender, msg.value);
+    }
+
+    function send(address payable recipient, uint amount) public payable{
         require(msg.sender == owner, "Transaction denied: sender is has no permissions!");
-        require(owner.balance < amount, "Transaction denied: not enough wei to send!");
-
+        require(owner.balance >= amount, "Transaction denied: not enough wei to send!");
+        recipient.transfer(amount);
         emit Withdraw(owner, amount);
     }
 
-    function send() public payable {}
-
-    function currentBalance() public view returns(address, uint256){ 
+    function info() public view returns(address, uint256){ 
         require(msg.sender == owner, "Request denied: Caller is not the owner");
         return (owner, address(this).balance);
     }
