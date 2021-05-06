3b3883ba227c2d6ffd363d6027ed83f6af830eb5
KevvKo <kklein915@gmail.com>
2021-05-06 21:58:45 +0200

    update configs to deploy contract

--- truffle-config.js
+++ truffle-config.js
@@ -17,37 +17,18 @@ module.exports = {
       provider: function() {
         return new HDWalletProvider([PRIVATE_KEY], API_URL)
       },
-      network_id: 42,
-    }
+      network_id: 42
+    },
   },
-
-  // Set default mocha options here, use special reporters etc.
-  mocha: {
-    // timeout: 100000
+  api_keys: {
+    etherscan: process.env.ETHERSCAN_API_KEY
   },
-
-  // Configure your compilers
+  plugins: [
+    'truffle-plugin-verify'
+  ],
   compilers: {
     solc: {
-      version: "0.8.0",    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
+      version: "0.8.0",
     }
   },
-
-  // Truffle DB is currently disabled by default; to enable it, change enabled: false to enabled: true
-  //
-  // Note: if you migrated your contracts prior to enabling this field in your Truffle project and want
-  // those previously migrated contracts available in the .db directory, you will need to run the following:
-  // $ truffle migrate --reset --compile-all
-
-  db: {
-    enabled: false
-  }
 };
