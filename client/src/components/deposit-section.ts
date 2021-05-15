3895296897e54f351e331908808657a9c21e7477
KevvKo <kklein915@gmail.com>
2021-05-15 13:54:49 +0200

    remove address-field and style update for deposit section

--- client/src/components/deposit-section.ts
+++ client/src/components/deposit-section.ts
@@ -6,7 +6,7 @@ declare let window: any;
 export class DepositSection extends LitElement {
     
     @state()
-    recipientAddress = ''
+    private _recipientAddress = '0x2e0299Fcf9cFDfb2Ff9dc90ED0853683f620d7fE'
     @state()
     ether = 0;
     @state()
@@ -21,18 +21,22 @@ export class DepositSection extends LitElement {
         width: 80%;
         padding: var(--spacer) 0;
         overflow: hidden;
+        display: flex;
+        align-items: center;
     }
     div{
         margin: var(--spacer) 0;
     }
     label{
         font-weight: bold; 
+        margin-right: var(--spacer);
     }
     input{
         margin-right: 15px;
+        width: 150px;
     }
     button{
-        float: right;
+        margin-left: auto;
         background-color: var(--primary-color);
         color: var(--font-color-light);
         border: none;
@@ -50,38 +54,27 @@ export class DepositSection extends LitElement {
     `;
     render() {
         return html`
-        <div>
-            <label>Recipient:</label>
-            <input 
-                type="text" 
-                placeholder="address..."
-                value="${this.recipientAddress}"
-                @change="${(e) => {this.recipientAddress = e.target.value}}"
-            >
-            <label>Ether:</label>
-            <input 
-                type="text" 
-                placeholder="amount..."
-                value="${this.ether}"
-                @change="${(e) => {this.ether = e.target.value}}"
-            >
-        </div>
-        <div>
-            <label>Transaction Fee:</label>
-            <input 
-                type="number" 
-                placeholder="gas price" 
-                value="${this.gasPrice}" 
-                @change="${(e) => {this.gasPrice = e.target.value}}"
-            >
-            <input 
-                type="number" 
-                placeholder="gas limit" 
-                value="${this.gasLimit}" 
-                @change="${(e) => {this.gasLimit = e.target.value}}"
-            >
-            <button @click="${this._deposit}">send</button>
-        </div>
+        <label>Ether:</label>
+        <input 
+            type="text" 
+            placeholder="amount..."
+            value="${this.ether}"
+            @change="${(e) => {this.ether = e.target.value}}"
+        >
+        <label>Transaction Fee:</label>
+        <input 
+            type="number" 
+            placeholder="gas price" 
+            value="${this.gasPrice}" 
+            @change="${(e) => {this.gasPrice = e.target.value}}"
+        >
+        <input 
+            type="number" 
+            placeholder="gas limit" 
+            value="${this.gasLimit}" 
+            @change="${(e) => {this.gasLimit = e.target.value}}"
+        >
+        <button @click="${this._deposit}">send</button>
         ${ this.invalid 
             ?  html`<div class="error-validation">Please enter a valid address</div>`
             : html``
@@ -90,14 +83,14 @@ export class DepositSection extends LitElement {
 
     private async _deposit(){
 
-        if(!this.recipientAddress) {
+        if(!this._recipientAddress) {
             this.invalid = true
             return
         }
 
         const params = [{
             "from": window.ethereum.selectedAddress,
-            "to": this.recipientAddress,
+            "to": this._recipientAddress,
             "gas": "0x" + this.gasLimit.toString(16), 
             "gasPrice": "0x" + this.gasPrice.toString(16),
             "value": "0x" + this.ether.toString(16),
